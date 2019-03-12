8d02ee5b87abac6fe50484dda7369077f6d58a94
Lyndon Fox <lyndonfox.web@gmail.com>
2019-03-12 17:29:47 +1300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld/>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Blog from "./components/Blog"
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Blog
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
