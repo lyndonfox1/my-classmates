a067706dbb280090e2cbe6dfe9f3d7ca0002165c
Matthew Kennett-Ny <matthewkennettny@gmail.com>
2019-03-13 17:42:31 +1300

    added hotair ballon pic

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
       <router-link v-bind:to="'/jack'">Jack</router-link>
+      <router-link v-bind:to="'/matthew'">Jack</router-link>
   </nav>
 </template>
 
---
+++ src/components/matthew/Matthew.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="https://cdn.pixabay.com/photo/2017/05/21/15/14/balloon-2331488_960_720.jpg">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Matthew",
+  components: {
+
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
